05100724ca5438c1d524ea28c1406990c213a136
Aditi Madaan <amadaan@BLR.ALLEGISINDIA.COM>
2022-04-08 19:55:10 +0530

    THIS IS MY PHASE1 PROJECT

---
+++ src/PhaseOneProject/OutputFiles.java
@@ -0,0 +1,15 @@
+package PhaseOneProject;
+
+public class OutputFiles
+{
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO LockedMe.com");
+		System.out.println("Developer -> Aditi ");
+		System.out.println("Company Name -> TEKsystems Global Services");
+		
+		
+		ChoosingOptions.display();
+		
+
+	}
+}
